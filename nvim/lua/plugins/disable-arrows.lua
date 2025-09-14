31cd67b3d596f636ca33e00397182cde920b8836
Cristian Manoliu <cristian.manoliu@gmail.com>
2025-09-14 22:56:37 +0300

    nvim - disable arrows

--- nvim/lua/plugins/disable-arrows.lua
+++ nvim/lua/plugins/disable-arrows.lua
@@ -3,7 +3,8 @@ return {
   {
     "LazyVim/LazyVim", -- attach to an existing, always-present plugin
     keys = function()
-      local modes = { "n", "i", "v", "x", "s", "o", "c", "t" }
+      -- local modes = { "n", "i", "v", "x", "s", "o", "c", "t" }
+      local modes = { "n", "i", "v", "x", "s", "o", "t" }
       local defs = {}
       for _, key in ipairs({ "<Up>", "<Down>", "<Left>", "<Right>" }) do
         table.insert(defs, { key, "<Nop>", mode = modes })
