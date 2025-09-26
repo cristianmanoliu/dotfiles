6090f36ca799aae3a6221c9a76c6a4475b8a11fa
Cristian Manoliu <cristian.manoliu@gmail.com>
2025-09-26 09:59:24 +0300

    NVIM - Keymaps

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,31 +1,40 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
--- Function to compile/run based on file extension
+-- Utility: extract CLI args only if current line has ARGS:
+local function extract_args()
+  local line = vim.fn.getline(".")
+  -- Match ARGS line: supports #, //, --, etc.
+  local m = line:match("^%s*[-/#;*]+%s*ARGS:%s*(.+)$")
+  if not m then
+    m = line:match("^%s*ARGS:%s*(.+)$")
+  end
+  return m or "" -- empty string if no ARGS on current line
+end
+
+-- Main runner: chooses command by extension and appends args only if present
 local function run_file()
   local ext = vim.fn.expand("%:e")
   local filename = vim.fn.expand("%:t")
   local basename = vim.fn.expand("%:t:r")
-  local cmd = ""
 
+  local args = extract_args()
+  local args_part = (args ~= "" and (" " .. args)) or ""
+
+  local cmd
   if ext == "java" then
-    cmd = string.format("javac %s && java %s", filename, basename)
+    cmd = string.format("javac %s && java %s%s", filename, basename, args_part)
   elseif ext == "py" then
-    cmd = string.format("python3 %s", filename)
+    cmd = string.format("python3 %s%s", filename, args_part)
   elseif ext == "sh" then
-    cmd = string.format("bash %s", filename)
+    cmd = string.format("bash %s%s", filename, args_part)
   elseif ext == "c" then
-    cmd = string.format("gcc %s -o %s && ./%s", filename, basename, basename)
+    cmd = string.format("gcc %s -o %s && ./%s%s", filename, basename, basename, args_part)
   else
-    print("No run command defined for ." .. ext .. " files")
+    print("No run command defined for extension: ." .. (ext or ""))
     return
   end
 
-  -- vim.cmd("split | terminal " .. cmd)
   -- Run in split terminal and auto-enter Terminal mode
   vim.cmd("split | terminal " .. cmd)
   vim.cmd("startinsert")
 end
 
--- Keymap to run the function
-vim.keymap.set("n", "<leader>r", run_file, { desc = "Compile & Run File", silent = true })
+vim.keymap.set("n", "<leader>r", run_file, { desc = "Compile & Run (ARGS if on line)", silent = true })
